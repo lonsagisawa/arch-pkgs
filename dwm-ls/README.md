d43198b8c9789d0616f2b1b9257f9c5772be9e1d
Lon Sagisawa <lon.sagisawa@outlook.jp>
2020-11-18 22:14:34 +0900

    whole new dwm package

---
+++ dwm-ls/README.md
@@ -0,0 +1,2 @@
+# dwm
+Heavily patched dwm for noob-friendliness and sane defaults.
--- dwm-ls/config.h
+++ dwm-ls/config.h
@@ -1,22 +1,45 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 6;        /* vertical padding for statusbar */
+static const char *fonts[]          = { "Sarasa Term J Semibold:size=10", 
+                                        "Font Awesome 5 Brands Regular:size=10",
+                                        "Font Awesome 5 Free Regular:size=10",
+                                        "Font Awesome 5 Free Solid:size=10" }; /* Font Awesome for icons */
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* Do you have favorite terminal color scheme, or even your crafted one? Great!
+   Bring their color code here and dwm will have that colors! */
+static const char col_bg[]          = "#282a36";
+static const char col_fg[]          = "#f8f8f2";
+static const char col_current[]     = "#44475a";
+static const char col_cyan[]        = "#8be9fd";
+static const char col_green[]       = "#50fa7b";
+static const char col_orange[]      = "#ffb86c";
+static const char col_pink[]        = "#ff79c6";
+static const char col_purple[]      = "#bd93f9";
+static const char col_red[]         = "#ff5555";
+static const char col_yellow[]      = "#f1fa8c";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_bg },
+	[SchemeSel]  = { col_fg, col_purple,  col_purple  },
+	[SchemeStatus]  = { col_fg, col_bg, "#000000" }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_purple, col_current, "#000000" }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_fg, col_bg, "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_fg, col_bg,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_fg, col_bg,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+
 };
 
 /* tagging */
@@ -28,29 +51,24 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            0,           0 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	/* { "[M]",      monocle }, */
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
-#define XF86AudioMute 0x1008ff12
-#define XF86AudioLowerVolume 0x1008ff11
-#define XF86AudioRaiseVolume 0x1008ff13
-#define XF86MonBrightnessDown 0x1008ff03
-#define XF86MonBrightnessUp 0x1008ff02
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -61,51 +79,81 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *brightnessup[] = { "brightnessControl", "up", NULL };
-static const char *brightnessdown[] = { "brightnessControl", "down", NULL };
-static const char *volumeup[] = { "volumeControl", "up", NULL };
-static const char *volumedown[] = { "volumeControl", "down", NULL };
-static const char *volumemute[] = { "volumeControl", "mute", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
+static const char *termcmd[]  = { "st", "-e", "fish", NULL };
 
-
+/* Keybindings
+   Heavily inspired from defaults of bspwm and awesomewm */
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,             {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,             {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,         {0} },
-	{ MODKEY,                       XK_j,      focusstack,        {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,        {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,          {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,          {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,              {0} },
-	{ MODKEY,                       XK_Tab,    view,              {0} },
-	{ MODKEY,                       XK_w,      killclient,        {0} },
-	{ MODKEY,                       XK_space,  setlayout,         {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating,    {0} },
-	{ MODKEY,                       XK_0,      view,              {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,               {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,          {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,          {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,            {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,            {.i = +1 } },
-	{ 0,                            XF86MonBrightnessUp,  spawn, {.v = brightnessup } },
-	{ 0,                            XF86MonBrightnessDown,spawn, {.v = brightnessdown } },
-	{ 0,                            XF86AudioLowerVolume ,spawn, {.v = volumedown } },
-	{ 0,                            XF86AudioRaiseVolume ,spawn, {.v = volumeup } },
-	{ 0,                            XF86AudioMute,        spawn, {.v = volumemute } },
-	TAGKEYS(                        XK_1,                         0)
-	TAGKEYS(                        XK_2,                         1)
-	TAGKEYS(                        XK_3,                         2)
-	TAGKEYS(                        XK_4,                         3)
-	TAGKEYS(                        XK_5,                         4)
-	TAGKEYS(                        XK_6,                         5)
-	TAGKEYS(                        XK_7,                         6)
-	TAGKEYS(                        XK_8,                         7)
-	TAGKEYS(                        XK_9,                         8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,              {0} },
+	/* Spawn rofi and terminal
+	   rofi: Super-d (d stands for dmenu, planning to move on dmenu)
+	   terminal: Super-Enter (st by default) */
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	/* Toggle bar 
+	   Super-b */
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	/* Moving around windows
+	   Super-Shift-j/k */
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
+	/* Moving focus
+	   Super-j/k */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	/* Increase and decrease windows in master(left side)
+	   Super-u/i */
+	{ MODKEY,                       XK_u,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = -1 } },
+	/* Resize master stack
+	   Super-h/l */
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	/* Swap focused window to master (it's called "zoom" in dwm world)
+	   Super-Shift-Enter */
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	/* Switch to previous used tag
+	   Super-Tab */
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	/* Close focused window
+	   Super-q (stolen from macOS defaults, Super-Shift-c is more comfortable for most UNIX users?) */
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	/* Switch layout
+	   Tiling: Super-t (stands for Tile)
+	   Floating: Super-f (stands for Froating)
+	   Toggle: Super-Space */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	/* Floating toggle for focused window
+	   Super-Shift-Space */
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_m,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	/* Multi-monitor support */
+	/* I use dwm on laptop so don't care about multi-monitor, 
+	   if you felt uncomfortable feel free to change bindings */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	/* Quit and restart
+	   Quit: Super-Shift-q
+	   Restart: Super-Shift-r */
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
 };
 
 /* button definitions */
